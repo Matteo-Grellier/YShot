597efa27dac438f755ca5103d625a4e1cd80fb1f
matheoleger <71370253+matheoleger@users.noreply.github.com>
2022-04-18 15:01:46 +0200

    feature(app.css, index.blade.php):Added font + change image resolution

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,12 +6,12 @@
 @section('content')
     <div class="manager-link-container">
         <div class="manager-link-element" onclick="location.href='{{ route('admin.manage_ingredients') }}'">
-            <img src="./img/fruits.png" width="150px" height="100%"/>
+            <img src="./img/fruits.png" width="150px"/>
             <h2>Gérer les ingrédients</h2>
             <p>Vous pourrez gérer vos listes d'ingrédients comme les softs, les alcools, les fruits etc...</p>
         </div>
         <div class="manager-link-element" onclick="location.href='{{ route('admin.manage_cocktails') }}'">
-            <img src="./img/cocktail.png" width="150px" height="100%"/>
+            <img src="./img/cocktail.png" width="150px"/>
             <h2>Gérer les cocktails</h2>
             <p>Vous pourrez gérer les cocktails en modifiant leur composition et en créant des nouveaux</p>
         </div>
